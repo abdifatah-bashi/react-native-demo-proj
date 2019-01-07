a466f6e8fb79afe787c2f476a8e922c9adb0665e
Abdifatah A. Bashi <fitah22@gmail.com>
2019-01-07 12:33:33 +0100

    Lagret nytt notat form

---
+++ src/components/NotatDetaljer.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Container, Button, Header,Textarea, Content, Form, Item, Input } from 'native-base';
+
+
+export default class NotatDetaljer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <Container style={styles.container}>
+      <Header />
+      <Content padder>
+        <Form>
+          <Item>
+            <Input placeholder="Titel" />
+          </Item>
+
+          <Item >
+           <View style={styles.textContainer}>
+           <Textarea rowSpan={10} bordered placeholder="Content" />
+           </View>
+             
+          </Item>
+
+          <Button block success>
+            <Text>Save</Text>
+          </Button>
+        </Form>
+      </Content>
+    </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    
+  },
+  textContainer:{
+    width: '100%'
+  }
+  
+});
